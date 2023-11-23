75e9e9cd80804cff67695abebe4caafdb322da44
Mariana Capo <macapo78@gmail.com>
2023-11-23 13:52:55 -0500

    styling login form and patients

--- src/components/Patient/Patient.js
+++ src/components/Patient/Patient.js
@@ -2,6 +2,7 @@ import './Patient.scss';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import ImgLanguage from '../../assets/images/child-speaking.svg';
+import ImgKidsPlaying from '../../assets/images/kids-playing.svg';
 
 const Patient = ({patientId, userId, namePatient, lastNamePatient, native_language_id, language}) => {
 
@@ -13,11 +14,13 @@ const Patient = ({patientId, userId, namePatient, lastNamePatient, native_langua
                     <p className="article-language-text">Speak: { language }</p>
                 </div>
                 <div className="article-welcome">
-                    <h3 className="article-welcome-text">Welcome</h3>
+                    <h2 className="article-welcome-text">Welcome</h2>
                     <h4 className="article-welcome-name">{namePatient} {lastNamePatient}</h4>
-                    <button className="article-welcome-button">
-                    Let's play
-                    </button>
+                    <div className="article-welcome-button">
+                        <img className='article-welcome-button-img' src={ ImgKidsPlaying } alt="Child Playing" />
+                        <p>Let's play</p>
+                        <img className='article-welcome-button-img' src={ ImgKidsPlaying } alt="Child Playing" />
+                    </div>
                 </div>
             </article>
         </Link>
