2561fff1bda2ec6d50e8b1767c110b124cf3faf4
Mariana Capo <macapo78@gmail.com>
2023-11-24 11:27:09 -0500

    added data about working words

--- src/components/SubNav/SubNav.js
+++ src/components/SubNav/SubNav.js
@@ -1,17 +1,23 @@
 import "./SubNav.scss";
 import categories from '../../assets/images/categories_icon.png';
 import { Link } from "react-router-dom";
+import imgWordActive from '../../assets/images/word-collection-icon.svg';
 
 const SubNav = () => {
+    const quizName = sessionStorage.getItem('quizName');
 
     return (
         <nav className="subnav">
             <Link to="/vocabulary">
                 <section className="subnav__categories">
                     <img src={categories} alt="Categories icon" className='subnav__categories-img' />
-                    Categories
+                    Word Collection
                 </section>
             </Link>
+            <section className="subnav__active">
+                <img src={imgWordActive} alt="Word Active Collection" className="subnav__active-img"></img>
+                You're learning: {quizName}
+            </section>
         </nav>
     );
 };
--- src/pages/QuizPage/QuizPage.js
+++ src/pages/QuizPage/QuizPage.js
@@ -11,7 +11,7 @@ import SubNav from "../../components/SubNav/SubNav";
 const QuizPage = () => {
     const {idQuiz, idUser, idWord} = useParams();
     const [wordSelected, setWordSelected] = useState();
-    
+
     useEffect(() => {
         if (idWord !== "0" && idWord !== undefined) {
             console.log(idWord, "estoy en quiz page id word")            
@@ -21,7 +21,7 @@ const QuizPage = () => {
     const [wordList, setWordList] = useState([]);
 
     useEffect(() => {
-        const fetchQuizData = async (idQuiz) => {
+        const fetchQuizData = async () => {
             try{
                 const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/quizzes/${idQuiz}`);
                 const data = response.data;
@@ -37,7 +37,7 @@ const QuizPage = () => {
                 console.log("Error fetching data quiz. Page Quiz.", error);
             }
         }
-        fetchQuizData(idQuiz);
+        fetchQuizData();
     }, [wordSelected]);
 
 
